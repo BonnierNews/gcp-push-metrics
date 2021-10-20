4778d416d52f6390780851e99fbfe27d0f83f314
Joel Abrahamsson <joel.abrahamsson@bonniernews.se>
2021-10-20 10:54:51 +0200

    dist for 2.0.0

--- index.cjs
+++ index.cjs
@@ -303,6 +303,63 @@ function Summary(config) {
   return { observe, startTimer, intervalReset, toTimeSeries };
 }
 
+async function CloudRunResourceProvider() {
+  const project_id = await request("/computeMetadata/v1/project/project-id");
+  const locationResponse = await request("/computeMetadata/v1/instance/region");
+  let instance_id = await request("/computeMetadata/v1/instance/id");
+  const splitLocation = locationResponse.split("/");
+  const location = splitLocation[splitLocation.length - 1];
+  return {
+    default: {
+      type: "generic_node",
+      labels: {
+        project_id,
+        namespace: process.env.K_SERVICE,
+        node_id: instance_id,
+        location,
+      },
+    },
+    exit: {
+      type: "generic_node",
+      labels: {
+        project_id,
+        namespace: process.env.K_SERVICE,
+        node_id: `${instance_id}-exit`,
+        location,
+      },
+    },
+  };
+}
+
+function request(path) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: "metadata.google.internal",
+      port: 80,
+      path,
+      method: "GET",
+      timeout: 200,
+      headers: {
+        "Metadata-Flavor": "Google",
+      },
+    };
+    const req = http__default["default"].request(options, (resp) => {
+      let data = "";
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      resp.on("end", () => {
+        resolve(data);
+      });
+    });
+    req.on("error", (err) => {
+      reject(err);
+    });
+    req.end();
+  });
+}
+
 function PushClient({ intervalSeconds, logger, resourceProvider } = {}) {
   if (intervalSeconds < 1) {
     throw new Error("intervalSeconds must be at least 1");
@@ -384,63 +441,6 @@ function PushClient({ intervalSeconds, logger, resourceProvider } = {}) {
   process.on("SIGTERM", push.bind(null, true));
 
   return { Counter: counter, Gauge: gauge, Summary: summary };
-}
-
-async function CloudRunResourceProvider() {
-  const project_id = await request("/computeMetadata/v1/project/project-id");
-  const locationResponse = await request("/computeMetadata/v1/instance/region");
-  let instance_id = await request("/computeMetadata/v1/instance/id");
-  const splitLocation = locationResponse.split("/");
-  const location = splitLocation[splitLocation.length - 1];
-  return {
-    default: {
-      type: "generic_node",
-      labels: {
-        project_id,
-        namespace: process.env.K_SERVICE,
-        node_id: instance_id,
-        location,
-      },
-    },
-    exit: {
-      type: "generic_node",
-      labels: {
-        project_id,
-        namespace: process.env.K_SERVICE,
-        node_id: `${instance_id}-exit`,
-        location,
-      },
-    },
-  };
-}
-
-function request(path) {
-  return new Promise((resolve, reject) => {
-    const options = {
-      hostname: "metadata.google.internal",
-      port: 80,
-      path,
-      method: "GET",
-      timeout: 200,
-      headers: {
-        "Metadata-Flavor": "Google",
-      },
-    };
-    const req = http__default["default"].request(options, (resp) => {
-      let data = "";
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      resp.on("end", () => {
-        resolve(data);
-      });
-    });
-    req.on("error", (err) => {
-      reject(err);
-    });
-    req.end();
-  });
 }
 
 exports.CloudRunResourceProvider = CloudRunResourceProvider;
